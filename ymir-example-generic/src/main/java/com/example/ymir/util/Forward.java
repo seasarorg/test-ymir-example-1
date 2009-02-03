e48835fb44d7b5736ca7aa15b9231b1d4bd20622
YOKOTA, Takehiko <skirnir@gmail.com>
2009-02-03 15:43:40 +0000

    最初のバージョン。
    git-svn-id: https://www.seasar.org/svn/ymir/example/trunk@2744 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-example-generic/src/main/java/com/example/ymir/util/Forward.java
@@ -0,0 +1,18 @@
+package com.example.ymir.util;
+
+import org.seasar.ymir.Response;
+import org.seasar.ymir.response.ForwardResponse;
+
+public class Forward {
+    protected Forward() {
+    }
+
+    public static Response to(String path, Object... params) {
+        return new ForwardResponse(PageUtils.constructPath(path, false, params));
+    }
+
+    public static Response to(Class<?> pageClass, Object... params) {
+        return new ForwardResponse(PageUtils.constructPath(pageClass, false,
+                params));
+    }
+}
---
+++ ymir-example-generic/src/main/java/com/example/ymir/util/Redirect.java
@@ -0,0 +1,28 @@
+package com.example.ymir.util;
+
+import org.seasar.ymir.Response;
+import org.seasar.ymir.response.RedirectResponse;
+
+public class Redirect {
+    protected Redirect() {
+    }
+
+    public static Response to(String path, Object... params) {
+        return new RedirectResponse(PageUtils
+                .constructPath(path, false, params));
+    }
+
+    public static Response to(Class<?> pageClass, Object... params) {
+        return new RedirectResponse(PageUtils.constructPath(pageClass, false,
+                params));
+    }
+
+    public static Response toNonCached(String path, Object... params) {
+        return new RedirectResponse(PageUtils.constructPath(path, true, params));
+    }
+
+    public static Response toNonCached(Class<?> pageClass, Object... params) {
+        return new RedirectResponse(PageUtils.constructPath(pageClass, true,
+                params));
+    }
+}
