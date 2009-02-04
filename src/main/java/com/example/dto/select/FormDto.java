ff01094a9f8945db3644a79a40edf4c3f2ccabd6
YOKOTA, Takehiko <skirnir@gmail.com>
2009-02-04 09:38:59 +0000

    ドロップダウンリストのサンプルを追加した。
    git-svn-id: https://www.seasar.org/svn/ymir/example/trunk/ymir-example-generic@2753 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ src/main/java/com/example/dto/select/FormDto.java
@@ -0,0 +1,11 @@
+package com.example.dto.select;
+
+import java.io.Serializable;
+
+public class FormDto extends FormDtoBase
+    implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public FormDto() {
+    }
+}
---
+++ src/main/java/com/example/web/select/IndexPageBase.java
@@ -0,0 +1,49 @@
+package com.example.web.select;
+
+@SuppressWarnings("unchecked")
+public class IndexPageBase extends com.example.web.PageBase {
+    @org.seasar.ymir.annotation.Meta(name="property",value="form")
+    protected com.example.dto.select.FormDto form = new com.example.dto.select.FormDto();
+
+    protected com.example.dto.select.ViewDto view = new com.example.dto.select.ViewDto();
+
+
+    public com.example.dto.select.FormDto getForm() {
+        return this.form;
+    }
+
+    @org.seasar.ymir.annotation.Meta(name="formProperty",value="form")
+    public String getFruits() {
+        return this.form.getFruits();
+    }
+
+    @org.seasar.ymir.annotation.Meta(name="formProperty",value="form")
+    @org.seasar.ymir.scope.annotation.RequestParameter(actionName={},name="",populateWhereNull=true)
+    public void setFruits(String fruits) {
+        this.form.setFruits(fruits);
+    }
+
+    public com.example.dto.select.ViewDto getView() {
+        return this.view;
+    }
+
+    public void _get() {
+
+    }
+
+    @org.seasar.ymir.annotation.Meta(name="source",value={"throw ex;","ex"})
+    @org.seasar.ymir.conversation.annotation.Begin(alwaysBegin=true)
+    public void _permissionDenied(org.seasar.ymir.constraint.PermissionDeniedException ex)
+        throws org.seasar.ymir.constraint.PermissionDeniedException {
+        throw ex;
+    }
+
+    public void _prerender() {
+
+    }
+
+    @org.seasar.ymir.annotation.Meta(name="source",value={"","notes"})
+    public void _validationFailed(org.seasar.ymir.message.Notes notes) {
+        
+    }
+}
