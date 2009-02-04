d305ee277cd86fe8f5f37519c2ca8729774ca70b
YOKOTA, Takehiko <skirnir@gmail.com>
2009-02-04 16:24:41 +0000

    ラジオボタンのサンプルを追加した。
    git-svn-id: https://www.seasar.org/svn/ymir/example/trunk@2755 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-example-generic/src/main/java/com/example/dto/radio/FormDto.java
@@ -0,0 +1,11 @@
+package com.example.dto.radio;
+
+import java.io.Serializable;
+
+public class FormDto extends FormDtoBase
+    implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public FormDto() {
+    }
+}
