c288acd1d90ab84a70932b318d94ac479e9871b6
YOKOTA, Takehiko <skirnir@gmail.com>
2009-02-25 05:56:27 +0000

    サブカンバセーションのサンプルを完成させた。
    git-svn-id: https://www.seasar.org/svn/ymir/example/trunk@2842 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-example-generic/src/main/java/com/example/web/register/InputPage.java
+++ ymir-example-generic/src/main/java/com/example/web/register/InputPage.java
@@ -3,10 +3,7 @@ package com.example.web.register;
 import org.seasar.ymir.Response;
 import org.seasar.ymir.conversation.annotation.Begin;
 import org.seasar.ymir.conversation.annotation.Conversation;
-import org.seasar.ymir.conversation.impl.ConversationScope;
-import org.seasar.ymir.scope.annotation.Out;
 
-import com.example.dto.register.FormDto;
 import com.example.ymir.util.Redirect;
 
 // 入力画面です。
@@ -23,12 +20,8 @@ public class InputPage extends InputPageBase {
         return Redirect.to(ConfirmPage.class);
     }
 
+    // 確認画面から戻ってくるためのアクションです。
+    // 戻った際にカンバセーションスコープに保存されているフォームを破棄してしまわないようにこうしています。
     public void _get_retry() {
-    }
-
-    @Out(ConversationScope.class)
-    @Override
-    public FormDto getForm() {
-        return super.getForm();
     }
 }
