7f5ca993579b1e870063a104502f8def9adcc872
YOKOTA, Takehiko <skirnir@gmail.com>
2009-02-27 03:55:27 +0000

    実験。
    git-svn-id: https://www.seasar.org/svn/ymir/example/trunk@2863 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-example-generic/src/main/java/com/example/web/checkbox/IndexPageBase.java
+++ ymir-example-generic/src/main/java/com/example/web/checkbox/IndexPageBase.java
@@ -1,11 +1,15 @@
 package com.example.web.checkbox;
 
 public class IndexPageBase extends com.example.web.PageBase {
+    protected com.example.dto.checkbox.ViewDto view = new com.example.dto.checkbox.ViewDto();
+
     @org.seasar.ymir.annotation.Meta(name="property",value="form")
     protected com.example.dto.checkbox.FormDto form = new com.example.dto.checkbox.FormDto();
 
-    protected com.example.dto.checkbox.ViewDto view = new com.example.dto.checkbox.ViewDto();
 
+    public com.example.dto.checkbox.ViewDto getView() {
+        return this.view;
+    }
 
     public com.example.dto.checkbox.FormDto getForm() {
         return this.form;
@@ -22,11 +26,11 @@ public class IndexPageBase extends com.example.web.PageBase {
         this.form.setFruits(fruits);
     }
 
-    public com.example.dto.checkbox.ViewDto getView() {
-        return this.view;
+    public void _get() {
+
     }
 
-    public void _get() {
+    public void _prerender() {
 
     }
 
@@ -34,10 +38,6 @@ public class IndexPageBase extends com.example.web.PageBase {
     public void _permissionDenied(org.seasar.ymir.constraint.PermissionDeniedException ex)
         throws org.seasar.ymir.constraint.PermissionDeniedException {
         throw ex;
-    }
-
-    public void _prerender() {
-
     }
 
     @org.seasar.ymir.annotation.Meta(name="source",value={"","notes"})
