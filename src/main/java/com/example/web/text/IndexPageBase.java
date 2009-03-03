d05ffcae6eaf880a9afb8b2b2c10586a3ab484a0
YOKOTA, Takehiko <skirnir@gmail.com>
2009-03-03 12:28:31 +0000

    バリデーションエラーが発生した際にハイライトするようにした。
    git-svn-id: https://www.seasar.org/svn/ymir/example/trunk/ymir-example-generic@2882 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- src/main/java/com/example/web/text/IndexPageBase.java
+++ src/main/java/com/example/web/text/IndexPageBase.java
@@ -1,20 +1,32 @@
 package com.example.web.text;
 
 public class IndexPageBase extends com.example.web.PageBase {
-    @org.seasar.ymir.annotation.Meta(name="property",value="form")
+    @org.seasar.ymir.annotation.Meta(name = "property", value = "form")
     protected com.example.dto.text.FormDto form = new com.example.dto.text.FormDto();
 
 
-    @org.seasar.ymir.annotation.Meta(name="formProperty",value="form")
-    public java.util.Date getDate() {
-        return this.form.getDate();
+    @org.seasar.ymir.annotation.Meta(name = "formProperty", value = "form")
+    public java.util.Date getDate1() {
+        return this.form.getDate1();
     }
 
-    @org.seasar.ymir.annotation.Meta(name="formProperty",value="form")
-    @org.seasar.ymir.constraint.annotation.Datetime(pattern="",property={},value="yyyyMMdd")
-    @org.seasar.ymir.scope.annotation.RequestParameter(actionName={},name="",populateWhereNull=true)
-    public void setDate(java.util.Date date) {
-        this.form.setDate(date);
+    @org.seasar.ymir.annotation.Meta(name = "formProperty", value = "form")
+    @org.seasar.ymir.constraint.annotation.Datetime("yyyyMMdd")
+    @org.seasar.ymir.scope.annotation.RequestParameter
+    public void setDate1(java.util.Date date1) {
+        this.form.setDate1(date1);
+    }
+
+    @org.seasar.ymir.annotation.Meta(name = "formProperty", value = "form")
+    public java.util.Date getDate2() {
+        return this.form.getDate2();
+    }
+
+    @org.seasar.ymir.annotation.Meta(name = "formProperty", value = "form")
+    @org.seasar.ymir.constraint.annotation.Datetime("yyyyMMdd")
+    @org.seasar.ymir.scope.annotation.RequestParameter
+    public void setDate2(java.util.Date date2) {
+        this.form.setDate2(date2);
     }
 
     public void _get() {
