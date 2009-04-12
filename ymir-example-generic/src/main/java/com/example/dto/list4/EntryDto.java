669569d58b6f66b6d3934f8d410336e74f8ef8dd
YOKOTA, Takehiko <skirnir@gmail.com>
2009-04-12 17:51:39 +0000

    Selectorクラスを使ったサンプルを作成中.
    git-svn-id: https://www.seasar.org/svn/ymir/example/trunk@3202 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ ymir-example-generic/src/main/java/com/example/dto/list4/EntryDto.java
@@ -0,0 +1,11 @@
+package com.example.dto.list4;
+
+import java.io.Serializable;
+
+public class EntryDto extends EntryDtoBase
+    implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public EntryDto() {
+    }
+}
--- ymir-example-generic/src/main/java/com/example/web/IndexPageBase.java
+++ ymir-example-generic/src/main/java/com/example/web/IndexPageBase.java
@@ -9,7 +9,7 @@ public class IndexPageBase extends com.example.web.PageBase {
 
     public static final String A_get = "_get";
 
-    @org.seasar.ymir.annotation.Meta(name = "bornOf", value = {"/exceptionHandler/index.html", "/aaa/index.html", "/ncheckbox/index.html", "/checkbox/index.html", "/checkbox2/index.html", "/radio/index.html", "/select/index.html", "/select2/index.html", "/list3/index.html"})
+    @org.seasar.ymir.annotation.Meta(name = "bornOf", value = {"/exceptionHandler/index.html", "/aaa/index.html", "/ncheckbox/index.html", "/checkbox/index.html", "/checkbox2/index.html", "/radio/index.html", "/select/index.html", "/select2/index.html", "/list3/index.html", "/list4/index.html"})
     public void _get() {
 
     }
