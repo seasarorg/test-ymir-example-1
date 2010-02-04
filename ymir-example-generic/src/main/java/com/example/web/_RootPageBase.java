540160e0b8d1aeeb48549fb138add8887bfda5f5
YOKOTA, Takehiko <skirnir@gmail.com>
2010-02-04 01:49:56 +0000

    Eclipse3.5.1でWarningが出ていた箇所を修正した。
    git-svn-id: https://www.seasar.org/svn/ymir/example/trunk@3671 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- ymir-example-generic/src/main/java/com/example/web/_RootPageBase.java
+++ ymir-example-generic/src/main/java/com/example/web/_RootPageBase.java
@@ -1,9 +1,8 @@
 package com.example.web;
 
-@SuppressWarnings("unchecked")
 public class _RootPageBase extends com.example.web.PageBase {
 
-    @org.seasar.ymir.annotation.Meta(name="source",value="return \"redirect:index.html\";")
+    @org.seasar.ymir.annotation.Meta(name = "source", value = "return \"redirect:index.html\";")
     public String _get() {
         return "redirect:index.html";
     }
